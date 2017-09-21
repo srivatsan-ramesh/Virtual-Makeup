ea6f64beacc95950f3fa4202dd858c80b2a883f7
srivatsan-ramesh <sriramesh4@gmail.com>
2017-09-22 01:06:21 +0530

    fixing indexing errors

--- lips/lipstick.py
+++ lips/lipstick.py
@@ -1,73 +1,76 @@
-
-from scipy.interpolate import interp1d
 from pylab import *
-from skimage import io, color
-r,g,b = (207.,40.,57.)		#lipstick color
+from scipy.interpolate import interp1d
+from skimage import color
+
+r, g, b = (207., 40., 57.)  # lipstick color
 
 up_left_end = 3
 up_right_end = 5
 
-def inter(lx=[],ly=[],k1='quadratic'):
-	unew = np.arange(lx[0], lx[-1]+1, 1)
-	f2 = interp1d(lx, ly, kind=k1)
-	return (f2,unew)
+
+def inter(lx, ly, k1='quadratic'):
+    unew = np.arange(lx[0], lx[-1] + 1, 1)
+    f2 = interp1d(lx, ly, kind=k1)
+    return f2, unew
+
 
 # gets the points on the boundary of lips from the file
 file = np.loadtxt('pointpixel.txt')
-points =  np.floor(file)
-point_out_x = np.array((points[:len(points)/2][:,0]))
-point_out_y = np.array(points[:len(points)/2][:,1])
-point_in_x = (points[len(points)/2:][:,0])
-point_in_y = points[len(points)/2:][:,1]
+points = np.floor(file)
+point_out_x = np.array((points[:len(points) // 2][:, 0]))
+point_out_y = np.array(points[:len(points) // 2][:, 1])
+point_in_x = (points[len(points) // 2:][:, 0])
+point_in_y = points[len(points) // 2:][:, 1]
 
 figure()
 im = imread('Input.jpg')
 
 # Code for the curves bounding the lips
-o_u_l = inter(point_out_x[:up_left_end],point_out_y[:up_left_end])
-o_u_r = inter(point_out_x[up_left_end-1:up_right_end],point_out_y[up_left_end-1:up_right_end])
-o_l = inter([point_out_x[0]]+point_out_x[up_right_end-1:][::-1].tolist(),[point_out_y[0]]+point_out_y[up_right_end-1:][::-1].tolist(),'cubic')
-i_u_l = inter(point_in_x[:up_left_end],point_in_y[:up_left_end])
-i_u_r = inter(point_in_x[up_left_end-1:up_right_end],point_in_y[up_left_end-1:up_right_end])
-i_l = inter([point_in_x[0]]+point_in_x[up_right_end-1:][::-1].tolist(),[point_in_y[0]]+point_in_y[up_right_end-1:][::-1].tolist(),'cubic')
+o_u_l = inter(point_out_x[:up_left_end], point_out_y[:up_left_end])
+o_u_r = inter(point_out_x[up_left_end - 1:up_right_end], point_out_y[up_left_end - 1:up_right_end])
+o_l = inter([point_out_x[0]] + point_out_x[up_right_end - 1:][::-1].tolist(),
+            [point_out_y[0]] + point_out_y[up_right_end - 1:][::-1].tolist(), 'cubic')
+i_u_l = inter(point_in_x[:up_left_end], point_in_y[:up_left_end])
+i_u_r = inter(point_in_x[up_left_end - 1:up_right_end], point_in_y[up_left_end - 1:up_right_end])
+i_l = inter([point_in_x[0]] + point_in_x[up_right_end - 1:][::-1].tolist(),
+            [point_in_y[0]] + point_in_y[up_right_end - 1:][::-1].tolist(), 'cubic')
 
-x = []	#will contain the x coordinates of points on lips
-y = []  #will contain the y coordinates of points on lips
+x = []  # will contain the x coordinates of points on lips
+y = []  # will contain the y coordinates of points on lips
 
-def ext(a,b,i):
-	a,b=round(a),round(b)
-	x.extend(arange(a,b,1).tolist())
-	y.extend((ones(b-a)*i).tolist())
 
-for i in range(int(o_u_l[1][0]),int(i_u_l[1][0]+1)):
-	ext(o_u_l[0](i),o_l[0](i)+1,i)
+def ext(a, b, i):
+    a, b = np.round(a), np.round(b)
+    x.extend(arange(a, b, 1, dtype=np.int32).tolist())
+    y.extend((ones(int(b - a), dtype=np.int32) * i).tolist())
 
-for i in range(int(i_u_l[1][0]),int(o_u_l[1][-1]+1)):
-	ext(o_u_l[0](i),i_u_l[0](i)+1,i)
-	ext(i_l[0](i),o_l[0](i)+1,i)
 
-for i in range(int(i_u_r[1][-1]),int(o_u_r[1][-1]+1)):
-	ext(o_u_r[0](i),o_l[0](i)+1,i)
+for i in range(int(o_u_l[1][0]), int(i_u_l[1][0] + 1)):
+    ext(o_u_l[0](i), o_l[0](i) + 1, i)
 
-for i in range(int(i_u_r[1][0]),int(i_u_r[1][-1]+1)):
-	ext(o_u_r[0](i),i_u_r[0](i)+1,i)
-	ext(i_l[0](i),o_l[0](i)+1,i)
+for i in range(int(i_u_l[1][0]), int(o_u_l[1][-1] + 1)):
+    ext(o_u_l[0](i), i_u_l[0](i) + 1, i)
+    ext(i_l[0](i), o_l[0](i) + 1, i)
+
+for i in range(int(i_u_r[1][-1]), int(o_u_r[1][-1] + 1)):
+    ext(o_u_r[0](i), o_l[0](i) + 1, i)
+
+for i in range(int(i_u_r[1][0]), int(i_u_r[1][-1] + 1)):
+    ext(o_u_r[0](i), i_u_r[0](i) + 1, i)
+    ext(i_l[0](i), o_l[0](i) + 1, i)
 
 # Now x and y contains coordinates of all the points on lips
 
-val = color.rgb2lab((im[x,y]/255.).reshape(len(x),1,3)).reshape(len(x),3)
-L,A,B = mean(val[:,0]),mean(val[:,1]),mean(val[:,2])
-L1,A1,B1 = color.rgb2lab(np.array((r/255.,g/255.,b/255.)).reshape(1,1,3)).reshape(3,)
-ll,aa,bb = L1-L,A1-A,B1-B
-val[:,0] += ll
-val[:,1] += aa
-val[:,2] += bb
-#val[:,0] = np.clip(val[:,0], 0, 100)
-#val[:,1] = np.clip(val[:,1], -127, 128)
-#val[:,2] = np.clip(val[:,2], -127, 128)
+val = color.rgb2lab((im[x, y] / 255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
+L, A, B = mean(val[:, 0]), mean(val[:, 1]), mean(val[:, 2])
+L1, A1, B1 = color.rgb2lab(np.array((r / 255., g / 255., b / 255.)).reshape(1, 1, 3)).reshape(3, )
+ll, aa, bb = L1 - L, A1 - A, B1 - B
+val[:, 0] += ll
+val[:, 1] += aa
+val[:, 2] += bb
 
-im[x,y] = color.lab2rgb(val.reshape(len(x),1,3)).reshape(len(x),3)*255
+im[x, y] = color.lab2rgb(val.reshape(len(x), 1, 3)).reshape(len(x), 3) * 255
 gca().set_aspect('equal', adjustable='box')
 imshow(im)
 show()
-imsave('output.jpg',im)
+imsave('output.jpg', im)
--- nails/nail.py
+++ nails/nail.py
@@ -2,85 +2,94 @@ from scipy import interpolate
 from pylab import *
 from skimage import color
 
-Rg, Gg, Bg = (207.,40.,57.)
+Rg, Gg, Bg = (207., 40., 57.)
 
 texture_input = 'texture1.jpg'
 
-def getBoundaryPoints(x, y):
-	tck,u = interpolate.splprep([x, y], s=0, per=1)
-	unew = np.linspace(u.min(), u.max(), 1000)
-	xnew,ynew = interpolate.splev(unew, tck, der=0)
-	tup = c_[xnew.astype(int),ynew.astype(int)].tolist()
-	coord = list(set(tuple(map(tuple, tup))))
-	coord = np.array([list(elem) for elem in coord])
-	return coord[:,0],coord[:,1]
-	
-def getInteriorPoints(x, y):
-	nailx = []
-	naily = []
-	def ext(a, b, i):
-		a, b=round(a), round(b)
-		nailx.extend(arange(a, b, 1).tolist())
-		naily.extend((ones(b-a)*i).tolist())
-	x, y = np.array(x), np.array(y)
-	xmin, xmax = amin(x), amax(x)
-	xrang = np.arange(xmin, xmax + 1, 1)
-	for i in xrang:
-		ylist = y[where(x==i)]
-		ext(amin(ylist), amax(ylist), i)
-	return nailx, naily
+
+def get_boundary_points(x, y):
+    tck, u = interpolate.splprep([x, y], s=0, per=1)
+    unew = np.linspace(u.min(), u.max(), 1000)
+    xnew, ynew = interpolate.splev(unew, tck, der=0)
+    tup = c_[xnew.astype(int), ynew.astype(int)].tolist()
+    coord = list(set(tuple(map(tuple, tup))))
+    coord = np.array([list(elem) for elem in coord])
+    return np.array(coord[:, 0], dtype=np.int32), np.array(coord[:, 1], dtype=np.int32)
+
+
+def get_interior_points(x, y):
+    nailx = []
+    naily = []
+
+    def ext(a, b, i):
+        a, b = round(a), round(b)
+        nailx.extend(arange(a, b, 1).tolist())
+        naily.extend((ones(b - a) * i).tolist())
+
+    x, y = np.array(x), np.array(y)
+    xmin, xmax = amin(x), amax(x)
+    xrang = np.arange(xmin, xmax + 1, 1)
+    for i in xrang:
+        ylist = y[where(x == i)]
+        ext(amin(ylist), amax(ylist), i)
+    return np.array(nailx, dtype=np.int32), np.array(naily, dtype=np.int32)
+
 
 im = imread('nail_inp.jpg')
 text = imread(texture_input)
-def applyNailPolish(x , y , r = Rg, g = Gg, b = Bg):
-	val = color.rgb2lab((im[x, y]/255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
-	L, A, B = mean(val[:,0]), mean(val[:,1]), mean(val[:,2])
-	L1, A1, B1 = color.rgb2lab(np.array((r/255., g/255., b/255.)).reshape(1, 1, 3)).reshape(3,)
-	ll, aa, bb = L1 - L, A1 - A, B1 - B
-	val[:, 0] = np.clip(val[:, 0] + ll, 0, 100)
-	val[:, 1] = np.clip(val[:, 1] + aa, -127, 128)
-	val[:, 2] = np.clip(val[:, 2] + bb, -127, 128)
-	im[x, y] = color.lab2rgb(val.reshape(len(x), 1, 3)).reshape(len(x), 3)*255
 
-def applyTexture(x, y):
-	xmin, ymin = amin(x),amin(y)
-	X = (x-xmin).astype(int)
-	Y = (y-ymin).astype(int)
-	val1 = color.rgb2lab((text[X, Y]/255.).reshape(len(X), 1, 3)).reshape(len(X), 3)
-	val2 = color.rgb2lab((im[x, y]/255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
-	L, A, B = mean(val2[:,0]), mean(val2[:,1]), mean(val2[:,2])
-	val2[:, 0] = np.clip(val2[:, 0] - L + val1[:,0], 0, 100)
-	val2[:, 1] = np.clip(val2[:, 1] - A + val1[:,1], -127, 128)
-	val2[:, 2] = np.clip(val2[:, 2] - B + val1[:,2], -127, 128)
-	im[x, y] = color.lab2rgb(val2.reshape(len(x), 1, 3)).reshape(len(x), 3)*255
+
+def apply_nail_polish(x, y, r=Rg, g=Gg, b=Bg):
+    val = color.rgb2lab((im[x, y] / 255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
+    L, A, B = mean(val[:, 0]), mean(val[:, 1]), mean(val[:, 2])
+    L1, A1, B1 = color.rgb2lab(np.array((r / 255., g / 255., b / 255.)).reshape(1, 1, 3)).reshape(3, )
+    ll, aa, bb = L1 - L, A1 - A, B1 - B
+    val[:, 0] = np.clip(val[:, 0] + ll, 0, 100)
+    val[:, 1] = np.clip(val[:, 1] + aa, -127, 128)
+    val[:, 2] = np.clip(val[:, 2] + bb, -127, 128)
+    im[x, y] = color.lab2rgb(val.reshape(len(x), 1, 3)).reshape(len(x), 3) * 255
+
+
+def apply_texture(x, y):
+    xmin, ymin = amin(x), amin(y)
+    X = (x - xmin).astype(int)
+    Y = (y - ymin).astype(int)
+    val1 = color.rgb2lab((text[X, Y] / 255.).reshape(len(X), 1, 3)).reshape(len(X), 3)
+    val2 = color.rgb2lab((im[x, y] / 255.).reshape(len(x), 1, 3)).reshape(len(x), 3)
+    L, A, B = mean(val2[:, 0]), mean(val2[:, 1]), mean(val2[:, 2])
+    val2[:, 0] = np.clip(val2[:, 0] - L + val1[:, 0], 0, 100)
+    val2[:, 1] = np.clip(val2[:, 1] - A + val1[:, 1], -127, 128)
+    val2[:, 2] = np.clip(val2[:, 2] - B + val1[:, 2], -127, 128)
+    im[x, y] = color.lab2rgb(val2.reshape(len(x), 1, 3)).reshape(len(x), 3) * 255
+
 
 points = np.loadtxt('nailpoint')
 
-x, y = points[:12, 0],points[:12, 1]
-x, y = getBoundaryPoints(x, y)
-x, y = getInteriorPoints(x, y)
-#applyNailPolish(x, y)
-applyTexture(x, y)
+x, y = points[:12, 0], points[:12, 1]
+x, y = get_boundary_points(x, y)
+x, y = get_interior_points(x, y)
+# applyNailPolish(x, y)
+apply_texture(x, y)
 
-x, y = points[12:24, 0],points[12:24, 1]
-x, y = getBoundaryPoints(x, y)
-x, y = getInteriorPoints(x, y)
-#applyNailPolish(x, y)
-applyTexture(x, y)
+x, y = points[12:24, 0], points[12:24, 1]
+x, y = get_boundary_points(x, y)
+x, y = get_interior_points(x, y)
+# applyNailPolish(x, y)
+apply_texture(x, y)
 
-x, y = points[24:36, 0],points[24:36, 1]
-x, y = getBoundaryPoints(x, y)
-x, y = getInteriorPoints(x, y)
-#applyNailPolish(x, y)
-applyTexture(x, y)
+x, y = points[24:36, 0], points[24:36, 1]
+x, y = get_boundary_points(x, y)
+x, y = get_interior_points(x, y)
+# applyNailPolish(x, y)
+apply_texture(x, y)
 
-x, y = points[36:, 0],points[36:, 1]
-x, y = getBoundaryPoints(x, y)
-x, y = getInteriorPoints(x, y)
-#applyNailPolish(x, y)
-applyTexture(x, y)
+x, y = points[36:, 0], points[36:, 1]
+x, y = get_boundary_points(x, y)
+x, y = get_interior_points(x, y)
+# applyNailPolish(x, y)
+apply_texture(x, y)
 
 figure()
 imshow(im)
-imsave('output_texture.jpg',im)
+imsave('output_texture.jpg', im)
 show()
